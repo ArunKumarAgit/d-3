83381f6eaf764ff62fa0cd2cb76218c9afd602af
Arun_kumar_A <arun.k@koinworx.com>
2023-02-11 14:43:30 +0530

    first commit

---
+++ 01.html selectors/script.js
@@ -0,0 +1,9 @@
+// const p = d3.select("h1");
+// p._groups[0][0].style.color = "red";
+// console.log(p);
+// const dat = (_) => {
+//   console.log("sdf");
+// };
+// const d = (_) => {
+//   console.log("s");
+// };
